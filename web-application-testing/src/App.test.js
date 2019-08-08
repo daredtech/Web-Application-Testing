73416e89e85e873cf214c9880437bbaab87cc095
Red <vhanchar@ucsc.edu>
2019-08-08 13:06:17 -0700

    checked if app renders without crashing

--- web-application-testing/src/App.test.js
+++ web-application-testing/src/App.test.js
@@ -1,9 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import "@testing-library/react/cleanup-after-each";
+import {render} from '@testing-library/react';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+
+describe('', () => {
+
+  // default
+  it('renders without crashing usung ReactDom', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+  // to see if it renders without crashing
+  it('should render the App component', () => {
+    render(<App />);
+
+  });
+
+})
+
+
+
