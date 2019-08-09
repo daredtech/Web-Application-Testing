dba8f4f4d8012b952def579035e651bee2a9ae8a
Red <vhanchar@ucsc.edu>
2019-08-09 00:08:38 -0700

    updated app and passed all the functions to props

--- web-application-testing/src/App.js
+++ web-application-testing/src/App.js
@@ -1,41 +1,56 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
-import Dashboard from './Components/Dashboard'
+import Dashboard from './Components/Dashboard';
+import Display from './Components/Display'
 
 function App() {
+
+const [balls, setBalls] = useState(0);
+const [strikes, setStrikes] = useState(0);
+
+function ballsCounting() {
+  console.log('inside ballsCounting');
+  if (balls >=4){
+      setBalls(0);
+      setStrikes(0);
+  }
+  else{
+      setBalls(balls+1);
+  }
+}
+
+function strikesCounting() {
+  console.log('inside strikesCounting');
+  if (strikes >=3){
+      setStrikes(0);
+      setBalls(0);
+  }
+  else{
+      setStrikes(strikes+1);
+  }
+}
+
+function foulsCounting() {
+  console.log('inside foulCounting');
+  if (strikes !== 2){
+      setStrikes(strikes+1);}
+  }
+
+function hitsCounting() {
+  console.log('inside hitCounting');
+  setBalls(0);
+  setStrikes(0);
+}
+
   return (
     <div className="App">
       <header className="App-header">
       </header>
-
-      <Dashboard/>
-
+      <Display strikes={strikes} balls={balls}/>
+      <Dashboard ballsCounting={ballsCounting} strikesCounting={strikesCounting} foulsCounting={foulsCounting} hitsCounting={hitsCounting} />
     </div>
   );
 }
 
 export default App;
 
-
-// const [balls, setBalls] = useState(0);
-// const [strikes, setStrikes] = useState(0);
-
-// function ballsCounting() {
-//     console.log('inside ballsCounting');
-//     if (balls >=4){
-//         setBalls(0);
-//     }
-//     else{
-//         setBalls(balls+1);
-//     }
-// }
-
-// function strikesCounting() {
-//     console.log('inside strikesCounting');
-//     if (strikes >=4){
-//         setStrikes(0);
-//     }
-//     else{
-//         setStrikes(strikes+1);
-//     }
-// }
