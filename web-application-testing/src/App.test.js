ecc830d593909969421d9586055081d1b7e8efb4
Red <vhanchar@ucsc.edu>
2019-08-09 00:26:35 -0700

    added tests fro ballsCounting

--- web-application-testing/src/App.test.js
+++ web-application-testing/src/App.test.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import "@testing-library/react/cleanup-after-each";
-import {render} from '@testing-library/react';
+import '@testing-library/react/cleanup-after-each';
+import {render, fireEvent} from '@testing-library/react';
 
 
 describe('', () => {
@@ -16,10 +16,26 @@ describe('', () => {
   // to see if it renders without crashing
   it('should render the App component', () => {
     render(<App />);
-    
   });
 
-})
+  // to check the ballsCounting functionality
+  it ('should match # clicked', () =>{
+    const {getByText} = render(<App />);
+    const ball = getByText(/^ball$/i); // button text
+  
+    fireEvent.click(ball);
+    fireEvent.click(ball);
+    getByText(/Balls: 2/);
+ 
+    // if over 4, should reset to 0
+    fireEvent.click(ball);
+    fireEvent.click(ball);
+    fireEvent.click(ball);
+    getByText(/Balls: 0/);
+  })
 
+
+
+})
 
 
